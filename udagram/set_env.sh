fecc88f5378e0c0c677529269f5e00d967680106
Tarek <Tarek@internal.com>
2022-12-08 08:25:43 +0200

    First Commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.c3of9rlrzweb.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udacity-udagram-tarek-aldahshan/archive.zip
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=abcdef
+export URL=http://udacityudagram-env.eba-p4ybyppz.us-east-1.elasticbeanstalk.com/
