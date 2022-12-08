88517df928156c98675f6a7b2f7a7eae5af34712
Tarek <Tarek@internal.com>
2022-12-08 09:03:21 +0200

    8

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  // apiHost: "http://udacity-udagram-tarek-aldahshan.s3-website-us-east-1.amazonaws.com/api/v0",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: 'Udagram',
+  // apiHost: 'http://udacity-udagram-tarek-aldahshan.s3-website-us-east-1.amazonaws.com/api/v0',
+  apiHost: 'http://localhost:8080/api/v0',
 };
 
 /*
