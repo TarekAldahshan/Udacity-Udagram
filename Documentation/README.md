c1b052015fe284b4754b63b6b72b00e663a21b60
Tarek <Tarek@internal.com>
2022-12-09 11:21:34 +0200

    09-12-2022:6

---
+++ Documentation/README.md
@@ -0,0 +1,16 @@
+# Deployment Training Application
+the main cause for building this application for studying how to deploy aplication on hosting server.
+this is training project
+it solving Hosting processes
+
+# Install Application 
+install nodejs v 16.17.1
+install front end dependencies
+install back end dependencies
+
+# Front End End Point
+
+# Licence 
+Open Source Applications From Udacity designed for training
+
+
--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,5 +1,5 @@
 aws s3 cp --recursive --acl public-read s3://udacity-udagram-tarek-aldahshan/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" s3://udacity-udagram-tarek-aldahshan/index.html
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./index.html s3://udacity-udagram-tarek-aldahshan/
 # aws s3 cp --recursive --acl public-read ./www s3://udacity-udagram-tarek-aldahshan/
 # aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udacity-udagram-tarek-aldahshan/
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT db_PORT=$db_PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
